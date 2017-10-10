ccdc88116f52bc5c5b8f856dc6674c9377f79bef
Karina Ionkina <kionkina@stuy.edu>
2017-10-10 18:38:20 -0400

    redirect fixed and jinja inheritance in use

---
+++ app.py
@@ -0,0 +1,43 @@
+#Team Urals                                                                
+#Karina Ionkina and Holden Higgins                                          
+#SoftDev1 pd7                                                                 
+#HW08-  Do I Know You?                                                      
+#2017-10-06  
+
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+user = "soft"
+paswd = "diva"
+ 
+
+app.secret_key = os.urandom(32)
+
+@app.route("/")
+def root():
+    if "username" in session:
+        return render_template("welcome.html")
+    else: 
+        return render_template("login.html")
+
+
+@app.route("/auth", methods = ["POST", "GET"])
+def authenticate():
+    if request.form != {}:
+        if request.form["user"] == user and request.form["paswd"] == paswd:
+            session['username'] = request.form["user"]
+    else:
+        ""
+    return redirect(url_for("root"))
+    
+
+@app.route("/logout")
+def logout():
+    session.pop("username")
+    return redirect(url_for("root"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
